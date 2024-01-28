3fb7a2450416399447b515c585c163329e208e8f
shokhjakhon01 <suyunovshohjahon08@gmail.com>
2024-01-29 03:01:08 +0500

    added sql dummy data for test

--- config/database.js
+++ config/database.js
@@ -58,7 +58,7 @@ module.exports = ({ env }) => {
         connectionString: env("DATABASE_URL"),
         host: env("DATABASE_HOST", "localhost"),
         port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "strapi"),
+        database: env("DATABASE_NAME", "strapiss"),
         user: env("DATABASE_USERNAME", "postgres"),
         password: env("DATABASE_PASSWORD", "12"),
         ssl: env.bool("DATABASE_SSL", false) && {
